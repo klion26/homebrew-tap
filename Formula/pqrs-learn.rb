c35b56990b967647b1db668508cc2d3e2b3d6e50
jreleaserbot <jreleaser@kordamp.org>
2025-02-08 03:55:37 +0000

    pqrs-learn v0.2.10

--- Formula/pqrs-learn.rb
+++ Formula/pqrs-learn.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.12.0 at 2024-06-01T13:16:51.147379448Z
+# Generated with JReleaser 1.12.0 at 2025-02-08T03:55:26.249270957Z
 
 class PqrsLearn < Formula
   desc "A command-line used to play with Apache Parquet file"
@@ -8,19 +8,19 @@ class PqrsLearn < Formula
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/klion26/pqrs-learn/releases/download/v0.2.10/pqrs-learn-0.2.10-aarch64-unknown-linux-gnu.zip"
-    sha256 "ee07029bbb11818eeb8eb06a0f9fcc3e6bb2d3016667919d90abce1898809925"
+    sha256 "8ae06317e6903724c09d3fa153c7155f654e3f9db6b336e561b6b98d1ed16819"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/klion26/pqrs-learn/releases/download/v0.2.10/pqrs-learn-0.2.10-x86_64-unknown-linux-gnu.zip"
-    sha256 "72af5e6ba78d0789833f83533424b09c89f2d6e267bbd32e459daaf251e9c294"
+    sha256 "089c1ffda5246b6dbe1fdc66a011c8fefb963eadfe4976122cf33343abef2053"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/klion26/pqrs-learn/releases/download/v0.2.10/pqrs-learn-0.2.10-aarch64-apple-darwin.zip"
-    sha256 "2fbd94cef16cff22450101731b450333860b84b1586cd478ab6ea466909ff0c6"
+    sha256 "25db57b401ad797066d256260494bf225b12ef56ff5a5d15fac421cdf34222a9"
   end
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/klion26/pqrs-learn/releases/download/v0.2.10/pqrs-learn-0.2.10-x86_64-apple-darwin.zip"
-    sha256 "273feaa34751205cfb5fae489cccefba2c85eaf41ff421ebe66ad9c71673a91d"
+    sha256 "5e35de1536c459055265dcc8e4d54179a9e6cf13c53428965f846b5130dfd807"
   end
 
 
